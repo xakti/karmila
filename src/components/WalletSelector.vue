ea85c4256a9fd4ded50e52b5abdc4e0ad17593b6
irwan <exploder.irwan@gmail.com>
2025-07-18 15:36:18 +0700

    ubah login dialog

--- src/components/WalletSelector.vue
+++ src/components/WalletSelector.vue
@@ -1,18 +1,29 @@
 <template>
-    <Dialog position="center" header="Wallet Selector" modal :visible="visible" @update:visible="visible = false"
-            class="max-w-9/10">
-        <div class="flex flex-col p-2 gap-2 text-center">
-            <span>Login dengan pindai kode QR atau klik tombol</span>
-            <Image preview :src="qrCodeSrc" class="shadow-md border border-surface rounded-xl p-1 md:w-[400px]">
-                <template #previewicon>
-                    <i class="pi pi-qrcode"></i>
-                </template>
-            </Image>
-            <Button class="w-fit mx-auto mt-2" text raised @click="openWallet">
-                <img alt="" src="https://herlina.web.app/herlina.png" width="32"/>
-                <span>Herlina</span>
-            </Button>
-        </div>
+    <Dialog modal :visible="visible" class="max-w-9/10" pt:root:class="!border-0 !bg-transparent">
+        <template #container>
+            <div class="flex flex-col login-dialog">
+                <div class="inline-flex justify-between items-center p-2">
+                    <span class="font-bold text-lg">Hubungkan Wallet</span>
+                    <Button text icon="pi pi-times" @click="visible = false"></Button>
+                </div>
+                <div class="flex flex-col mx-2 mb-2 gap-2 text-center">
+                    <span class="text-sm">Login dengan pindai kode QR atau klik tombol</span>
+                    <div class="inline-flex gap-2 mx-auto">
+                        <Button size="small" text raised @click="openWallet">
+                            <img alt="" src="/herlina.png" width="32"/>
+                            <span class="font-bold">Herlina</span>
+                        </Button>
+                        <Button text raised @click="copyRequest" icon="pi pi-clipboard"></Button>
+                    </div>
+                    <Image preview :src="qrCodeSrc"
+                           class="shadow-md border border-surface rounded-xl bg-white p-1 md:w-[400px]">
+                        <template #previewicon>
+                            <i class="pi pi-qrcode"></i>
+                        </template>
+                    </Image>
+                </div>
+            </div>
+        </template>
     </Dialog>
 </template>
 
@@ -20,10 +31,11 @@
 import {onMounted, ref} from "vue";
 import {useToast} from "primevue";
 import QrCodeWithLogo from "qrcode-with-logos";
-import Wallet from "@/js/wallet.js";
-import {abiCache, client} from "@/js/nodes.js";
+import Wallet from "../js/wallet.js";
+import {abiCache, client} from "../js/nodes.js";
 import {IdentityProof} from "@wharfkit/signing-request";
 import {Herlina, WalletSession} from "herlina-kit";
+import {copyToClipboard} from "../js/utils.js";
 
 const toast = useToast();
 const visible = ref(false);
@@ -32,24 +44,25 @@ let vsr = "";
 
 onMounted(() => {
     Wallet.show = show;
-    Wallet.hide = () => {
-        visible.value = false;
-    };
 });
 
 function show() {
     if (!Wallet.herlina) {
-        let herlina = new Herlina();
+        const herlina = new Herlina();
         herlina.on("error", onServerError);
         herlina.on("session", onSession);
+        vsr = herlina.createLoginRequest("Karmila", "https://karmila.web.app/icon-128.png");
         herlina.connect();
         Wallet.herlina = herlina;
-
-        vsr = herlina.createLoginRequest("Karmila", "https://karmila.web.app/icon-128.png");
-        initQRCode(vsr);
     }
 
+    initQRCode(vsr);
     visible.value = true;
+}
+
+function copyRequest() {
+    copyToClipboard(vsr);
+    toast.add({life: 3000, severity: "info", summary: "Login VSR disalin"});
 }
 
 async function initQRCode(content) {
@@ -87,34 +100,38 @@ function onServerError(error) {
 /**
  *
  * @param {WalletSession} session
- * @param {IdentityProof} proof
+ * @param {IdentityProof?} proof
  */
 async function onSession(session, proof) {
-    console.log("tersambung dengan wallet");
-
-    // verifikasi akun
-    const account = await client.v1.chain.get_account(proof.signer.actor);
-    if (proof.verify(account.getPermission(proof.signer.permission).required_auth)) {
-        Wallet.setAccount(proof.signer.toString());
-        toast.add({life: 3000, severity: "success", summary: `tersambung dengan ${proof.signer.toString()}`});
-
-        session.setABICache(abiCache);
-        session.onClose(onClose);
-        Wallet.session = session;
-
-        // putuskan signal server
-        Wallet.herlina.disconnect();
-        visible.value = false; // tutup dialog
+    if (proof && proof instanceof IdentityProof) {
+        // verifikasi akun
+        const account = await client.v1.chain.get_account(proof.signer.actor);
+        if (proof.verify(account.getPermission(proof.signer.permission).required_auth)) {
+            Wallet.setAccount(proof.signer.actor.toString());
+            toast.add({life: 3000, severity: "success", summary: `Tersambung dengan ${proof.signer.actor.toString()}`});
+        } else {
+            toast.add({life: 3000, severity: "error", summary: "Gagal verifikasi akun"});
+            Wallet.close();
+            return;
+        }
+    } else {
+        // login dari session
+        Wallet.setAccount(session.actor.toString());
+        toast.add({life: 3000, severity: "success", summary: `Tersambung dengan ${session.actor.toString()}`});
     }
+
+    session.setABICache(abiCache);
+    session.onClose(onClose);
+    Wallet.session = session;
+
+    // putuskan signal server
+    Wallet.herlina.disconnect();
+    visible.value = false; // tutup dialog
 }
 
 
 function onClose() {
-    console.log("putus dengan wallet");
-    Wallet.open.value = false;
-    Wallet.herlina.destroy();
-    Wallet.herlina = undefined;
-    Wallet.session = undefined;
+    Wallet.close();
     toast.add({life: 4000, severity: "info", summary: "Terputus dengan wallet"});
 }
 </script>
